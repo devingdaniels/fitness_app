eeaccce3190cdfbd4b8c4525aa5a95be011d079d
Devin Daniels <devingdaniels@gmail.com>
2022-10-17 12:53:27 -0700

    Added component files

--- fitness-ui/src/App.js
+++ fitness-ui/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+// Import dependencies 
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+// Components
+import Navigation from './components/Nav'
+import Footer from './components/Footer';
+
+// Pages
+import HomePage from './pages/HomePage'
+import CreateExercisePage from './pages/createExercisePage'
+import EditExercisePage from './pages/EditExercisePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <BrowserRouter>
+
+      <header>
+        <h1>Fitness Tracker App</h1>
+        <p>Add and track all your fitness exercises!</p>
       </header>
-    </div>
+
+      <Navigation/>
+
+      <main>
+      <Routes>
+        <Route>
+          <HomePage path="/" exact></HomePage>
+        </Route>
+        
+        <Route>
+          <CreateExercisePage path="add-exercise"></CreateExercisePage>
+        </Route>
+
+        <Route>
+          <EditExercisePage path="edit-exercise"></EditExercisePage>
+        </Route>
+        
+      </Routes>
+      </main>
+
+      <footer>
+        <Footer/>
+      </footer>
+
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ fitness-ui/src/pages/createExercisePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const createExercisePage = () => {
+  return (
+    <div>createExercisePage</div>
+  )
+}
+
+export default createExercisePage
