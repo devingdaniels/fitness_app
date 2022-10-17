6240ee1d22aefa5cbd183e3e47396b9e952f823b
Devin Daniels <devingdaniels@gmail.com>
2022-10-17 08:58:32 -0700

    Added the C in CRUD in the REST API fitness-controller.mjs

--- fitness-rest/fitness-controller.mjs
+++ fitness-rest/fitness-controller.mjs
@@ -10,10 +10,28 @@ const app = express();
 app.use(express.json());
 
 
-
-
-
-
+// CREATE controller
+app.post('/exercise', (req, res) => {
+    // Create a new instance of a movie by calling () in model layer
+    const name = req.body.name
+    const reps = req.body.reps
+    const weight = req.body.weight
+    const unit = req.body.unit
+    const date = req.body.date
+    // Returns a promise 
+    exercises.createExercise(name, reps, weight, unit, date)
+    // Send back code 201 (request success and resource created)
+    // Send back new json exercise object
+        .then(exercise => {
+            res.status(201).json(exercise)
+        })
+    // Catch failures 
+    // Send back status code 400 (server cannot or failed to process the request )
+        .catch(error =>{
+            console.log(error)
+            res.status(400).json({Error: "Creation of a document failed due to invalid syntax."})
+        })
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
